8d499c8f2c7e2bd12af4e4acaeb71e7eff13ee7d
TonnyMontana99 <anthonyigor2009@hotmail.com>
2023-06-03 22:08:00 -0600

    commit

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [isOn, setIsOn] = useState(false);
+  const handleClick = () => {
+    setIsOn(!isOn);
+  };
+
+  const botonestilo={
+    backgroundColor: isOn ? 'Green' : 'Red',
+    Color: 'black',
+    padding: '10px',
+    border: '1px solid',
+    borderRadius: '20px',
+    cursor : 'pointer',
+    
+  };
+  
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={handleClick} style ={botonestilo}>
+        {isOn ? 'On' : 'Off'}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
